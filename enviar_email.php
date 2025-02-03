67070c58b7c23cfef9a1064215e7890294585ff2
RichardTechOne <richard.souza@enelo.com.br>
2025-02-03 15:03:25 -0300

    Commit correção de imagens

--- enviar_email.php
+++ enviar_email.php
@@ -20,18 +20,18 @@
 
         // HTML do e-mail com interpolação de variáveis
         $htmlEmail = '<div style="max-width: 100%; margin: 0 auto; background-color: #ffffff; overflow: hidden; box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); font-family: Arial, sans-serif; background-color: #f4f4f4;">
-                            <div style="background-image: url(\'https://softone.techonebrasil.com.br/img/img_fundo_cabecalho_email.png\');background-repeat: round;padding: 51px;text-align:center;color:#ffffff;min-height: 90px;">
+                            <div style="background-image: url(\'https://techonebrasil.com.br/img/img_fundo_cabecalho_email.png\');background-repeat: round;padding: 51px;text-align:center;color:#ffffff;min-height: 90px;">
                             </div>
                             <div style="padding: 30px; text-align: center;">
                                 ' . $nome . ' deseja entrar em contato com a Tech One. Dados: ' . $ramo . ', ' . $telefone . ', ' . $mensagem . ', ' . $email  . '
                             </div>
                             <div style="background-color: #5e065a; padding: 10px 20px; color: #ffffff; text-align: center;">
                                 <p style="margin: 0;">Nos siga!</p>
-                                <a href="https://www.instagram.com/tech.one.br/" style="margin: 0 5px;"><img src="https://softone.techonebrasil.com.br/site-techone/img/icone_instagram.png" width="50" alt="Instagram"></a>
-                                <a href="https://www.facebook.com/profile.php?id=61560483996013" style="margin: 0 5px;"><img src="https://softone.techonebrasil.com.br/site-techone/img/icone_facebook.png" width="50" alt="Facebook"></a>
-                                <a href="https://www.linkedin.com/company/tech-one-br/?viewAsMember=true" style="margin: 0 5px;"><img src="https://softone.techonebrasil.com.br/site-techone/img/icone_linkedin.png" width="50" alt="Linkedin"></a>
-                                <a href="https://www.tiktok.com/@techone.oficial" style="margin: 0 5px;"><img src="https://softone.techonebrasil.com.br/site-techone/img/icone_tiktok.png" width="50" alt="TikTok"></a>
-                                <p style="margin: 10px 0;"><img src="https://softone.techonebrasil.com.br//img/logo-techone.png" width="20" style=" margin: -6px 0px; "> <a href="https://www.techonebrasil.com.br" style="color: #ffffff; text-decoration: none;">www.techonebrasil.com.br</a></p>
+                                <a href="https://www.instagram.com/tech.one.br/" style="margin: 0 5px;"><img src="https://techonebrasil.com.br/website/img/icone_instagram.png" width="50" alt="Instagram"></a>
+                                <a href="https://www.facebook.com/profile.php?id=61560483996013" style="margin: 0 5px;"><img src="https://techonebrasil.com.br/website/img/icone_facebook.png" width="50" alt="Facebook"></a>
+                                <a href="https://www.linkedin.com/company/tech-one-br/?viewAsMember=true" style="margin: 0 5px;"><img src="https://techonebrasil.com.br/website/img/icone_linkedin.png" width="50" alt="Linkedin"></a>
+                                <a href="https://www.tiktok.com/@techone.oficial" style="margin: 0 5px;"><img src="https://techonebrasil.com.br/website/img/icone_tiktok.png" width="50" alt="TikTok"></a>
+                                <p style="margin: 10px 0;"><img src="https://techonebrasil.com.br//img/logo-techone.png" width="20" style=" margin: -6px 0px; "> <a href="https://www.techonebrasil.com.br" style="color: #ffffff; text-decoration: none;">www.techonebrasil.com.br</a></p>
                             </div>
                         </div>';
 
